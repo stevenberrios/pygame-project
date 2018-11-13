ed26a4d774d70d51b7f18a85c7327aa1d4ecc090
Steven Berrios <berrios.steven.sb1@gmail.com>
2018-11-12 20:28:09 -0500

    Update ClumsyCadet.py

--- ClumsyCadet.py
+++ ClumsyCadet.py
@@ -1,54 +1,54 @@
-import sys
-import pygame
+import pygame, sys, random
+from pygame.locals import *
 
-def playgame():
+def init_main_window(dimensions, caption):
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
-    pygame.display.set_caption("Clumsy Cadet")
-    bgcolor = (200,200,200)
-    #create background
+    pygame.display.set_caption(caption)
+    return pygame.display.set_mode(dimensions)
+
+def load_cadet_images():
+    """
+    file_names = [List of all cadet gifs needed for animation]
+    cadet_images = []
+    for file_name in file_names:
+        cadet_img = pygame.image.load(file_name)
+        cadet_images.append(cadet_img)
+    return(cadet_images)
+    """
+    pass
+
+def move_cadet():
+    pass
+
+def hit_pipe():
+    pass
+
+def draw_text(text, font, display, x, y):
+    pass
+
+def play_game():
+
+    DISPLAY = init_main_window((400, 300), "Clumsy Cadet")
+
+    FPS = 15
+    fps_clock = pygame.time.Clock()
+
+    score = 0
+    font = pygame.font.SysFont(None, 20)
+
+    #event loop
     while True:
-        for i in pygame.event.get()
-            if i.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
                 sys.exit()
-        screen.fill(bgcolor)
-        pygame.display.flip()
-playgame()            
-    #https://stackoverflow.com/questions/41189928/pygame-how-to-change-background-color
-    #https://www.pygame.org/docs/ref/key.html
 
-#input image of the background, cadet, obstacles and music(if we can)
+        #draw a clean background
+        DISPLAY.fill(BLUE)
 
-#keyboard link (K_UP if not push any key auto drop)
+        # update display
+        pygame.display.update()
+        fps_clock.tick(FPS)
 
-#fix if the user pressing moving bottom it can keep moving (not stop)
-
-#scorerecord by pixels(maybe?????)
-def scorerecord():
-
-#showscore
-def showscore():
-    scoredigits= #index???
-
-#checkcollapse
-
-#creatpipe in the background
-
-#random apply pipes in background
-
-
-#mainfuction
-
-#set the center picture of the cadet touch the pipe will cause collapse instead of the margin
-
-#when collapse life -= 1 and reborn (3 lifes) 
-
-#after 3 lifes collapse return gameover screen
-
-#include the quit loop in every function so that user can stop game if they want 
-
-#write an pause function so that it can stop in the middle
-
-#(optional) count the time if it over 3 minutes return "YOU ARE THE CHAMP"
-
-#need to consider the background moving speed 
+if __name__ == '__main__':
+    play_game()

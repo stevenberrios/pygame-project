13d046b563db77b44a0326eff25951e0d71bb71f
Steven Berrios <berrios.steven.sb1@gmail.com>
2018-11-12 21:24:02 -0500

    Update ClumsyCadet.py

--- ClumsyCadet.py
+++ ClumsyCadet.py
@@ -1,36 +1,64 @@
-import pygame, sys, random
-from pygame.locals import *
+from itertools import cycle
+import random, sys, pygame
+
+GAMECAPTION = "Clumsy Cadet"
+SCREENWIDTH = 288
+SCREENHEIGHT = 512
+BASEY = SCREENHEIGHT * 0.79
+
+# GLOBAL CONSTANTS color definitions
+           #R    G    B
+ORANGE =   (255, 128, 0)
+BLUE =     (0,   0,   255)
+GREEN =    (0,   128, 0)
+PURPLE =   (128, 0,   128)
+RED =      (255, 0,   0)
+YELLOW =   (255, 255, 0)
+NAVYBLUE = (0,   0,   128)
+WHITE =    (255, 255, 255)
+BLACK =    (0,   0,   0)
+
+#images
+PLAYER = ()
+BACKGROUND = ()
+PIPE = ()
 
 def init_main_window(dimensions, caption):
     pygame.init()
     pygame.display.set_caption(caption)
     return pygame.display.set_mode(dimensions)
 
-def load_cadet_images():
-    """
-    file_names = [List of all cadet gifs needed for animation]
-    cadet_images = []
-    for file_name in file_names:
-        cadet_img = pygame.image.load(file_name)
-        cadet_images.append(cadet_img)
-    return(cadet_images)
-    """
+def move_cadet(cadet, event, dist, display_surf):
     pass
 
-def move_cadet():
+def check_collapse(cadet, upperPipe, lowerPipe):
     pass
 
-def hit_pipe():
-    pass
+def getRandomPipe():
+    """ Return randomly generated pipe """
+    # y gap between upper and lower pipe
+    gapY = random.randrange(0, int(BASEY * 0.6 - PIPEGAPSIZE))
+    gapY += int(BASEY * 0.2)
+    pipeHeight = PIPE.get_height()
+    pipeX = SCREENWIDTH + 10
 
-def draw_text(text, font, display, x, y):
-    pass
+    return[
+    {'x': pipeX, 'y': gapY - pipeHeight}, #upper pipe
+    {'x': pipeX, 'y': gapY + PIPEGAPSIZE}, #lower pipe
+    ]
+
+def draw_text(text, font, display_surf, x, y):
+    textobj = font.render(text, 1, WHITE)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    display_surf.blit(textobj, textrect)
+
 
 def play_game():
 
-    DISPLAY = init_main_window((400, 300), "Clumsy Cadet")
+    DISPLAY = init_main_window((SCREENWIDTH, SCREENHEIGHT), GAMECAPTION)
 
-    FPS = 15
+    FPS = 30
     fps_clock = pygame.time.Clock()
 
     score = 0
@@ -43,12 +71,14 @@ def play_game():
                 pygame.quit()
                 sys.exit()
 
-        #draw a clean background
+        # draw a clean background
         DISPLAY.fill(BLUE)
 
-        # update display
-        pygame.display.update()
-        fps_clock.tick(FPS)
+        draw_text(str(score), font, DISPLAY, 10, 0)
+
+    pygame.display.update()
+    fps_clock.tick(FPS)
 
 if __name__ == '__main__':
     play_game()
+
